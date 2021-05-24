3fcdda41a64d00558195b7c6caeb2cc85482eec5
kriguel <>
2021-05-24 19:19:52 +0000

    WIP. Feita a parte 1 e quase toda parte 2.

--- haskell01parte1.hs
+++ haskell01parte1.hs
@@ -1,2 +1,43 @@
 -- Prática 01 de Haskell - Parte 1
--- Nome: 
+-- Nome:  Leonardo Cargnin Krugel
+
+
+-- 1)
+sumSquares :: Int -> Int -> Int
+sumSquares n1 n2 = (n1^2) + (n2^2)
+
+-- 2)
+circleArea :: Float -> Float
+circleArea r = pi * (r^2)
+
+-- 3)
+age :: Int -> Int -> Int
+age year0 year1 = year1 - year0
+
+-- 4)
+isElderly :: Int -> Bool
+isElderly age = age > 65
+
+-- 5)
+htmlItem :: String -> String
+htmlItem str = "<li>" ++ str ++ "</li>"
+
+-- 6)
+startWithA :: String -> Bool
+startWithA str = head str == 'A'
+
+-- 7)
+isVerb :: String -> Bool
+isVerb str = last str == 'r'
+
+-- 8)
+isVowel :: Char -> Bool
+isVowel c = elem c ['a','e','i','o','u']
+
+-- 9)
+hasEqHeads :: [Int] -> [Int] -> Bool
+hasEqHeads list1 list2 = (head list1) == (head list2)
+
+-- 10)
+isVowel2 :: Char -> Bool
+isVowel2 c = elem c ['a','A','e','E','i','I','o','O','u','U']
--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,2 +1,74 @@
 -- Prática 01 de Haskell - Parte 2
--- Nome: 
+-- Nome: Leonardo Cargnin Krugel
+
+
+-- 1)
+htmlItem :: String -> String
+htmlItem str = "<li>" ++ str ++ "</li>"
+
+itemize :: [String] -> [String]
+itemize names = map htmlItem names
+
+
+-- 2)
+isVowel2 :: Char -> Bool
+isVowel2 c = elem c ['a','A','e','E','i','I','o','O','u','U']
+
+onlyVowels :: String -> String
+onlyVowels str = filter isVowel2 str
+
+
+-- 3)
+isElderly :: Int -> Bool
+isElderly age = age > 65
+
+onlyElderly :: [Int] -> [Int]
+onlyElderly list = filter isElderly list
+
+
+-- 4)
+isLongWord :: String -> Bool
+isLongWord s = length s > 10
+
+onlyLongWords :: [String] -> [String]
+onlyLongWords list = filter isLongWord list
+
+
+-- 5)
+isEven :: Int -> Bool
+isEven n = if mod n 2 == 0 then True else False
+
+onlyEven :: [Int] -> [Int]
+onlyEven list = filter isEven list
+
+
+-- 6)
+between60and80 :: Num -> Bool
+between60and80 n = (n > 60) && (n < 80)
+
+onlyBetween60and80 :: [Num] -> [Num]
+onlyBetween60and80 list = filter between60and80 list
+
+
+-- 7)
+isSpace :: Char -> Bool
+isSpace c = c == ' '
+
+onlySpaces :: [Char] -> [Char]
+onlySpaces str = filter isSpace str
+
+countSpaces :: String -> Int
+countSpaces str = length (onlySpaces str)
+
+
+-- 8)
+circleArea :: Float -> Float
+circleArea r = pi * (r^2)
+
+calcAreas :: [Float] -> [Float]
+calcAreas list = map circleArea list
+
+
+-- 9)
+charFound :: Char -> String -> Bool
+charFound c str = 
